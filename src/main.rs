8715940238100fe8cd9d7880d83da74cbc606d4f
george <george.duffett@icloud.com>
2023-01-02 01:23:56 +0000

    Add deserialize to main

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+mod journal;
+
+use std::fs;
+use std::error::Error;
+
+use serde_json;
+
+const JOURNAL_FILEPATH: &str = "ledger.json";
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let ledgerfile: String = fs::read_to_string(JOURNAL_FILEPATH)?.parse()?;
+    let input_journal: journal::Journal = serde_json::from_str(&ledgerfile)?;
+    println!("{:?}", input_journal);
+    Ok(())
+}
