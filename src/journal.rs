705a1efea056e463ffd8998540cb6083a703a34f
george <george.duffett@icloud.com>
2023-01-02 01:23:29 +0000

    Add simple journal fields

---
+++ src/journal.rs
@@ -0,0 +1,18 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Journal {
+    pub transactions: Vec<Transaction>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Transaction {
+    pub entries: Vec<Entry>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Entry {
+    pub account: String,
+    pub amount: f64,
+}
+
