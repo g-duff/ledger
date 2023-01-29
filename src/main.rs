da9b28102789ee61fdcdf88e40054e00b90e619f
george <george.duffett@icloud.com>
2023-01-29 23:49:12 +0000

    Fix main formatting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::error::Error;
 
-#[macro_use] extern crate prettytable;
-use clap::{Parser};
+#[macro_use]
+extern crate prettytable;
+use clap::Parser;
 
 mod cli;
 mod journal;
@@ -11,8 +12,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let user_input = cli::Cli::parse();
 
     match &user_input.command {
-        cli::Commands::Balance { filepath, from_date, to_date } => {
-            cli::balance_handler(filepath, from_date, to_date)
-        }
-    }    
+        cli::Commands::Balance {
+            filepath,
+            from_date,
+            to_date,
+        } => cli::balance_handler(filepath, from_date, to_date),
+    }
 }
