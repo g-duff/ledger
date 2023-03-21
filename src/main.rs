a7ab5cbcd69b6bf5fce9c099a63c4b17bf157dae
george <george.duffett@icloud.com>
2023-03-21 00:06:04 +0000

    Remove cli details from main file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
 #[macro_use]
 extern crate prettytable;
-use clap::Command;
 
+mod cli;
 mod commands;
 mod journal;
 mod report;
 
 fn main() {
-    let user_input = Command::new("ledger")
-        .subcommand(commands::balance::balance_command())
-        .get_matches();
-
-    match user_input.subcommand() {
-        Some(("balance", report_args)) => commands::balance::balance_handler(report_args),
-        _ => println!("no command given"),
-    }
+    cli::main();
 }
