c067c73500a08077efa29ac65cdbb3fdd6a90a53
george <george.duffett@icloud.com>
2023-05-20 16:44:26 +0100

    Change monzo, hsbc to use common sed date script

--- utils/hsbc-to-json.sh
+++ utils/hsbc-to-json.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-
-to_iso_date() {
-	sed -E 's/^([0-9]{2})\/([0-9]{2})\/([0-9]{4})/\3-\2-\1/' $1
-}
-
-to_iso_date $1 | sort | $PWD/awk/hsbc-to-json.awk -v account=$2
+./sed/to-iso-date.sed $1 | sort | ./awk/hsbc-to-json.awk -v account=$2
--- utils/monzo-to-json.sh
+++ utils/monzo-to-json.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-
-to_iso_date() {
-	sed -E 's/([0-9]{2})\/([0-9]{2})\/([0-9]{4})/\3-\2-\1/' $1
-}
-
-to_iso_date $1 | $PWD/awk/monzo-to-json.awk account=$2
+./sed/to-iso-date.sed $1 | ./awk/monzo-to-json.awk account=$2
