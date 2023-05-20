045a807aff7ef24699210476c7ecc8049ba87af1
george <george.duffett@icloud.com>
2023-05-20 13:43:43 +0100

    Change hsbc shell script to use awk file

--- utils/hsbc-to-json.sh
+++ utils/hsbc-to-json.sh
@@ -4,20 +4,4 @@ to_iso_date() {
 	sed -E 's/^([0-9]{2})\/([0-9]{2})\/([0-9]{4})/\3-\2-\1/' $1
 }
 
-to_json() {
-	awk -v account=$1 'BEGIN { FS="," }
-	{
-		amount = $3$4;
-		gsub(/\"/, "", amount);
-
-		print "{"
-		printf "\t\"date\": \"%s\",\n",$1
-		printf "\t\"description\": \"%s\",\n",$2
-		print "\t\"entries\": ["
-		printf "\t\t{\"account\": \"%s\", \"amount\": %.2f},\n",account,amount
-		printf "\t\t{\"account\": \"__\", \"amount\": %.2f}\n",(-1*amount)
-		print "\t]\n},"
-	}'
-}
-
-to_iso_date $1 | sort | to_json $2
+to_iso_date $1 | sort | $PWD/awk/hsbc-to-json.awk -v account=$2
