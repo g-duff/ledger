10222256e26ac1e0a6f9100a8cffd137f69ef275
george <george.duffett@icloud.com>
2023-05-21 13:52:52 +0100

    Change to dynamic awk and file patterns

--- utils/scratch.sh
+++ utils/scratch.sh
@@ -1,21 +1,18 @@
+# Use: ./utils/scratch.sh /Directory-of-csv-statements
+
 filename_to_accountname() {
 	basename $1 | sed 's/_/:/g; s/\.\///; s/.csv//'
 }
 
-hsbc() {
-	for filename in $1/*_hsbc_*.csv; do
-		[ -f $filename ] && ./utils/awk/from-hsbc.awk \
+process_bank_statements() {
+	for filename in $2/*_$1_*.csv; do
+		[ -f $filename ] && ./utils/awk/from-$1.awk \
 			-v account=$(filename_to_accountname $filename) \
 			$filename
 	done;
 }
 
-monzo() {
-	for filename in $1/*_monzo_*.csv; do
-		[ -f $filename ] && ./utils/awk/from-monzo.awk \
-			-v account=$(filename_to_accountname $filename) \
-			$filename
-	done;
-}
+alias process_hsbc='process_bank_statements hsbc'
+alias process_monzo='process_bank_statements monzo'
 
-(hsbc $1; monzo $1) | sort | ./utils/awk/to-json.awk
+(process_hsbc $1; process_monzo $1) | sort | ./utils/awk/to-json.awk
