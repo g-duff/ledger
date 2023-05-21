2eba2a5c830ecd441d056e3cc45ef1c1f584da49
george <george.duffett@icloud.com>
2023-05-21 13:21:33 +0100

    Remove need for sed iso date script

--- utils/scratch.sh
+++ utils/scratch.sh
@@ -3,19 +3,19 @@ filename_to_accountname() {
 }
 
 hsbc() {
-	for filename in $1*_hsbc_*.csv; do
-		[ -f $filename ] && ./bin/awk/from-hsbc.awk \
+	for filename in $1/*_hsbc_*.csv; do
+		[ -f $filename ] && ./utils/awk/from-hsbc.awk \
 			-v account=$(filename_to_accountname $filename) \
 			$filename
 	done;
 }
 
 monzo() {
-	for filename in $1*_monzo_*.csv; do
-		[ -f $filename ] && ./bin/awk/from-monzo.awk \
+	for filename in $1/*_monzo_*.csv; do
+		[ -f $filename ] && ./utils/awk/from-monzo.awk \
 			-v account=$(filename_to_accountname $filename) \
 			$filename
 	done;
 }
 
-(hsbc $1; monzo $1) | ./bin/sed/to-iso-date.sed | sort | ./bin/awk/to-json.awk; 
+(hsbc $1; monzo $1) | sort | ./utils/awk/to-json.awk
