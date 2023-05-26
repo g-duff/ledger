1793f45d760eaf5ab46f31c2cf8b0ef25e1ed5c4
george <george.duffett@icloud.com>
2023-05-27 00:20:11 +0100

    Allow dash and underscore in filenames

--- utils/scratch.sh
+++ utils/scratch.sh
@@ -1,12 +1,12 @@
 # Use: ./utils/scratch.sh /Directory-of-csv-statements
 
 filename_to_accountname() {
-	basename $1 | sed 's/_/:/g; s/\.\///; s/.csv//'
+	basename $1 | sed -E 's/-|_/:/g; s/\.\///; s/.csv//'
 }
 
 process_bank_statements() {
 	# Use: process_bank_statements bank-name csv-directory
-	for filename in $2/*_$1_*.csv; do
+	for filename in $2/*$1*.csv; do
 		[ -f $filename ] && ./utils/awk/from-$1.awk \
 			-v account=$(filename_to_accountname $filename) \
 			$filename
