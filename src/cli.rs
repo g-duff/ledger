b9f8eebdaf2ff602bbbbf4f2e58548c6b539993c
george <george.duffett@icloud.com>
2023-06-18 23:49:24 +0100

    feat: add register command to cli

--- src/cli.rs
+++ src/cli.rs
@@ -5,10 +5,12 @@ use crate::commands;
 pub fn main() {
     let user_input = command!()
         .subcommand(commands::balance::balance_command())
+        .subcommand(commands::register::register_command())
         .get_matches();
 
     match user_input.subcommand() {
         Some(("balance", report_args)) => commands::balance::balance_handler(report_args),
+        Some(("register", register_args)) => commands::register::register_handler(register_args),
         _ => println!("no command given"),
     }
 }
