2fed91155286849ee9a7421dcfafc63cf440f566
George Duffett <george.duffett@bbc.co.uk>
2026-02-10 19:06:18 +0000

    feat: add yaml file support

--- src/controllers/utils.rs
+++ src/controllers/utils.rs
@@ -1,12 +1,21 @@
 use std::error::Error;
 use std::fs;
+use std::path::Path;
+use std::ffi::OsStr;
 
 use crate::model::journal::Journal;
 
+pub fn load_journal(filepath: &String)  -> Result<Journal, Box<dyn Error>> {
 
-pub fn load_journal(filepath: &String) -> Result<Journal, Box<dyn Error>> {
+    let ext: Option<&str> = Path::new(filepath).extension().and_then(OsStr::to_str);
     let ledgerfile: String = fs::read_to_string(filepath)?.parse()?;
-    let input_journal: Journal = serde_json::from_str(&ledgerfile)?;
+    let input_journal: Journal = match ext {
+        Some("json") => serde_json::from_str(&ledgerfile)?,
+        Some("yaml") => serde_yaml::from_str(&ledgerfile)?,
+        Some("yml") => serde_yaml::from_str(&ledgerfile)?,
+        Some(&_) => panic!("Only file extensions json, yaml and yaml are supported"),
+        None => panic!("No file extension"),
+    };
     input_journal.validate();
 
     Ok(input_journal)
